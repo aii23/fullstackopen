8d7ad40606e1dca5e03f166c1332d1238c243d05
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2023-10-30 10:10:16 +0300

    Added error notification

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,14 +4,39 @@ import { InputForm } from "./InputForm";
 import { Contacts } from "./Contacts";
 import { Filter } from "./Filter";
 
+import "./index.css";
+
+const Notification = ({ cssClass, message }) => {
+    if (message === null) {
+        return null;
+    }
+
+    return <div className={cssClass}>{message}</div>;
+};
+
 const App = () => {
     const [persons, setPersons] = useState([]);
-
     const [filter, setFilter] = useState("");
+    const [message, setMessage] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         phonebookService.getAll().then((data) => setPersons(data));
     }, []);
+
+    const popMessage = (value) => {
+        setMessage(value);
+        setTimeout(() => {
+            setMessage(null);
+        }, 5000);
+    };
+
+    const popError = (value) => {
+        setError(value);
+        setTimeout(() => {
+            setError(null);
+        }, 5000);
+    };
 
     const addPerson = (newPerson) => {
         const existingPerson = persons.find(
@@ -34,6 +59,17 @@ const App = () => {
                                     : person
                             )
                         );
+
+                        popMessage(`Updated ${newPerson.name}`);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        popError("Contact is already removed");
+                        setPersons(
+                            persons.filter(
+                                (person) => person.id != existingPerson.id
+                            )
+                        );
                     });
             } else {
                 return Promise.resolve();
@@ -43,18 +79,29 @@ const App = () => {
                 .addContact(newPerson)
                 .then((returnedPerson) => {
                     setPersons(persons.concat(returnedPerson));
+                    popMessage(`Added ${newPerson.name}`);
                 });
         }
     };
 
     const removePerson = (id) => {
-        return phonebookService.removeContact(id).then(() => {
-            setPersons(persons.filter((person) => person.id != id));
-        });
+        return phonebookService
+            .removeContact(id)
+            .then(() => {
+                setPersons(persons.filter((person) => person.id != id));
+                popMessage(`Removed contact with id ${id}`);
+            })
+            .catch((error) => {
+                console.log(error);
+                popError("Contact is already removed");
+                setPersons(persons.filter((person) => person.id != id));
+            });
     };
 
     return (
         <div>
+            <Notification cssClass="notification" message={message} />
+            <Notification cssClass="notification error" message={error} />
             <h1>Phonebook</h1>
             <Filter filter={filter} setFilter={setFilter} />
             <h2>Enter new contact</h2>
